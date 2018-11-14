04ae73e7ab068d31b7ffde13960b6ef37395ba17
CheckMaters <39863034+CheckMaters@users.noreply.github.com>
2018-11-13 20:54:02 -0500

    Update movieListData.c

--- movieListData.c
+++ movieListData.c
@@ -1,50 +1,197 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include "generalFunctions.h"
 #include "columnsorter.h"
 #include "movieListData.h"
-#include "movieValueList.h"
+#include "hashTable.h"
+#include "tokenizer.h"
 #define TRUE				1
 #define FALSE				0
 
 
-//Inserting one movie to the movie list
+/*
+This function creates a list of movie
+where each pointer is movie with list of
+values attached to it.
+one_Movie_Values pointer is point to a specific
+movie with linked list of values attached to it
 
+This function also reads the type of the input value
+of each values attached to that movie.
+*/
+one_Movie_Values* make_Movie_Value_List(char* movie_Line, sorting_Column_Info* column_Information) {
 
-int add_One_Movie_To_The_List(movie_Record* pointer_To_MovieList_Array, one_Movie_Values* pointer_To_Movie) {
-	int output_Result = 1;
-	// checking inputs if they are correct or not
-	if (pointer_To_MovieList_Array == NULL || (pointer_To_MovieList_Array->pRecArray) == NULL || pointer_To_Movie==NULL) {
-		output_Result = 1;
-	}
-	else {
-		if (pointer_To_MovieList_Array->iCapacity > pointer_To_MovieList_Array->iSize) {
-			*(pointer_To_MovieList_Array->pRecArray + pointer_To_MovieList_Array->iSize) = pointer_To_Movie;
-			pointer_To_MovieList_Array->iSize++;
+		one_Movie_Values * temp_Pointer_To_Movie = (one_Movie_Values*)malloc(sizeof(one_Movie_Values) + strlen(movie_Line));
+		strcpy(temp_Pointer_To_Movie->recordData, movie_Line);						//copying the line from the file to temporary pointer
+	// Find sorting key
+	if (temp_Pointer_To_Movie != NULL) {
+		Tokenizer temp_word;
+		initialize_Tokenizer(&temp_word, movie_Line, ',', '\"');
 
-			output_Result = 0;
+		char* bufferStart = movie_Line;																// Pointer movie_Line will move along the string. Remember the start here.
+		char* temp_Word_Token;
+
+		temp_Word_Token = getNextToken(&temp_word);
+		int index = 0;
+		//
+		while(column_Information->index > index) {	// Go to (column_Information->index)th field, which is the sorting column word
+			// Key and token are not equal. Continue loop
+			temp_Word_Token = getNextToken(&temp_word);
+			index++;
+		}
+		//
+		int offset;
+		int length;																						// Length of the column word
+		if (temp_Word_Token == NULL) {
+			offset = strlen(temp_Pointer_To_Movie->recordData);
+			length = 0;
 		}
 		else {
-			pointer_To_MovieList_Array->pRecArray = (one_Movie_Values**)realloc(pointer_To_MovieList_Array->pRecArray, sizeof(one_Movie_Values) * (pointer_To_MovieList_Array->iCapacity + 1024));
-			pointer_To_MovieList_Array->iCapacity += 1024;
-			*(pointer_To_MovieList_Array->pRecArray + pointer_To_MovieList_Array->iSize) = pointer_To_Movie;
-			pointer_To_MovieList_Array->iSize++;
-
-			output_Result = 0;
+			offset = temp_Word_Token - bufferStart;
+			length = strlen(temp_Word_Token);
 		}
-	}
+		//
+		temp_Pointer_To_Movie->sorting_Key = temp_Pointer_To_Movie->recordData + offset;
+		temp_Pointer_To_Movie->sorting_Key_Term = temp_Pointer_To_Movie->recordData + offset + length;
+		temp_Pointer_To_Movie->chHold = *temp_Pointer_To_Movie->sorting_Key_Term;									// Remember the char at  temp_Pointer_To_Movie->sorting_Key_Term (to be reset later)
+		*temp_Pointer_To_Movie->sorting_Key_Term = '\0';															// Set temp_Pointer_To_Movie->sorting_Key_Term to null. Now temp_Pointer_To_Movie->pSKey is a proper string
+		//
+		temp_Pointer_To_Movie->long_Integer_Value = 0L;																			// Trying to change into long
+		temp_Pointer_To_Movie->floating_Value = 0.0f;																		// Trying to change into float
+		char	cTemp;
+		if (length > 0) {
+			char* pDot = strchr(temp_Pointer_To_Movie->sorting_Key, '.');
 
-	return output_Result;
+
+
+			if (column_Information->index_Type_SIF == INDEX_TYPE_INTEGER) {
+				if (!pDot && sscanf(temp_Pointer_To_Movie->sorting_Key, "%ld%c", &(temp_Pointer_To_Movie->long_Integer_Value), &cTemp) == 1) {				// integer
+					temp_Pointer_To_Movie->floating_Value = temp_Pointer_To_Movie->long_Integer_Value;
+				}
+				else if (pDot && sscanf(temp_Pointer_To_Movie->sorting_Key, "%f%c", &(temp_Pointer_To_Movie->floating_Value), &cTemp) == 1) {			// float
+					column_Information->index_Type_SIF = INDEX_TYPE_FLOAT;
+				}
+				else {
+					column_Information->index_Type_SIF = INDEX_TYPE_STRING;
+				}
+			}
+			else if (column_Information->index_Type_SIF == INDEX_TYPE_FLOAT) {
+				if (!pDot && sscanf(temp_Pointer_To_Movie->sorting_Key, "%ld%c", &(temp_Pointer_To_Movie->long_Integer_Value), &cTemp) == 1) {
+					temp_Pointer_To_Movie->floating_Value = temp_Pointer_To_Movie->long_Integer_Value;
+				}
+				else if (pDot && sscanf(temp_Pointer_To_Movie->sorting_Key, "%f%c", &(temp_Pointer_To_Movie->floating_Value), &cTemp) == 1) {
+
+				}
+				else {
+					column_Information->index_Type_SIF = INDEX_TYPE_STRING;
+				}
+			}
+			else {
+
+			}
+		}
+        hash_Struct * hash_Table = (hash_Struct *) malloc (sizeof(hash_Table) * 28);
+        create_Hash_Table(hash_Table);
+        
+        Tokenizer temp_Word_Two;
+        initialize_Tokenizer(&temp_Word_Two, movie_Line, ',', '\"');
+        char * temp_Word_Two_Token;
+        int temp_count = 1;
+        temp_Word_Two_Token = getNextToken(&temp_Word_Two);
+        
+        while (temp_Word_Two_Token != NULL){
+            temp_count++;
+            temp_Word_Two_Token = getNextToken(&temp_Word_Two);
+        }
+        
+        
+        char ** list_Of_Given_Column = (char **) malloc (sizeof(char *) * temp_count);
+        
+        Tokenizer temp_Word_Three;
+        initialize_Tokenizer(&temp_Word_Three, movie_Line, ',', '\"');
+        char * temp_Word_Three_Token;
+        
+        temp_count = 0;
+        while (temp_Word_Three_Token != NULL) {
+            temp_Word_Three_Token = getNextToken(&temp_Word_Three);
+            *(list_Of_Given_Column + temp_count) = (char *) malloc (sizeof(char) * strlen(temp_Word_Three_Token));
+            strcpy(*(list_Of_Given_Column + temp_count), temp_Word_Three_Token);
+            temp_count++;
+        }
+	}
+    
+    int hash_Key;
+    int temp_i = 0;
+    while (i < (temp_count+1)){
+        hash_Key = ((int)(*(list_Of_Given_Column + temp_i))) % 28;
+        
+    }
+
+    
+    
+    
+	return temp_Pointer_To_Movie;
 }
 
-void clearMovieList(movie_Record* pointer_To_MovieList_Array) {
-	if (pointer_To_MovieList_Array != NULL) {
-		int i;
-		for (i = 0; i < pointer_To_MovieList_Array->iSize; i++) {
-			free( (*(pointer_To_MovieList_Array->pRecArray + i)) );
-		}
 
-		free(pointer_To_MovieList_Array->pRecArray);
+
+int compare(void* pData1, void* pData2, void* pHint) {
+	one_Movie_Values* pRecord1 = (one_Movie_Values*)pData1;
+	one_Movie_Values* pRecord2 = (one_Movie_Values*)pData2;
+
+	int end_Result;			//return result, will change accordingly
+
+	sorting_Column_Info* column_Information = (sorting_Column_Info*)pHint;
+	int index_Type_SIF = column_Information->index_Type_SIF;
+
+	int len1 = strlen(pRecord1->sorting_Key);
+	int len2 = strlen(pRecord2->sorting_Key);
+	if (len1 == 0 && len2 > 0) {
+		end_Result = -1;
 	}
+	else if (len1 > 0 && len2 == 0) {
+		end_Result = 1;
+	}
+	else if (len1 == 0 && len2 == 0) {
+		end_Result = 0;
+	}
+	else {
+		long 	ldelta;
+		float fdelta;
+		switch(index_Type_SIF) {
+			case INDEX_TYPE_INTEGER:
+				ldelta = pRecord1->long_Integer_Value - pRecord2->long_Integer_Value;
+				if (ldelta == 0L) {
+					end_Result = 0;
+				}
+				else if (ldelta > 0L) {
+					end_Result = 1;
+				}
+				else {
+					end_Result = -1;
+				}
+				break;
+
+			case INDEX_TYPE_FLOAT :
+				fdelta = pRecord1->floating_Value - pRecord2->floating_Value;
+				if (fdelta == 0.0) {
+					end_Result = 0;
+				}
+				else if (fdelta > 0.0) {
+					end_Result = 1;
+				}
+				else {
+					end_Result = -1;
+				}
+				break;
+
+		   default :
+				end_Result = strcicmp(pRecord1->sorting_Key, pRecord2->sorting_Key);
+				break;
+		}
+	}
+	return end_Result;
 }
